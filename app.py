c5370b20087632b27a10cd5a3b55c24e0ec1b757
adam <zliu@conviva.com>
2020-09-02 17:16:32 +0800

    initial project

---
+++ app.py
@@ -0,0 +1,11 @@
+#https://realpython.com/flask-by-example-part-1-project-setup/
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
